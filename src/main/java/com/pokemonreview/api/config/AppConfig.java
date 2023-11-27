d74bef3c7a29dec39a0e06b08f8e44898e484ee6
onelovehj <onelovehj@hotmail.com>
2023-11-27 18:21:24 +0900

    initial commit

---
+++ src/main/java/com/pokemonreview/api/config/AppConfig.java
@@ -0,0 +1,70 @@
+package com.pokemonreview.api.config;
+
+import com.pokemonreview.api.models.Pokemon;
+import com.pokemonreview.api.models.PokemonType;
+import com.pokemonreview.api.models.Review;
+import com.pokemonreview.api.repository.PokemonRepository;
+import com.pokemonreview.api.repository.ReviewRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+//import org.modelmapper.ModelMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Configuration
+//@Profile("prod")
+public class AppConfig {
+    @Bean
+    public CommandLineRunner test(PokemonRepository pokemonRepository, ReviewRepository reviewRepository) {
+        return args -> {
+            System.out.println("**** Pokemon Insert 시작");
+            //pokemonRepository.deleteAll();
+            List<Pokemon> pokemonList = IntStream.rangeClosed(1, 10)
+                    .mapToObj(i -> Pokemon.builder()
+                            .name("pikachu" + i)
+                            .type(PokemonType.ELECTRIC)
+                            .build())
+                    .collect(Collectors.toList());
+
+            pokemonRepository.saveAll(pokemonList);
+            System.out.println("**** Pokemon Insert 끝");
+
+            System.out.println("#### Review Insert 시작");
+            pokemonRepository.findAll() //List<Pokemon>
+                            .forEach(pokemon -> {
+                                Review review = Review.builder()
+                                        .title(pokemon.getName() + " title")
+                                        .content(pokemon.getName() + " content")
+                                        .stars(5)
+                                        .pokemon(pokemon)
+                                        .build();
+                                reviewRepository.save(review);
+
+                                Review review2 = Review.builder()
+                                        .title(pokemon.getType().getName() + " title")
+                                        .content(pokemon.getType().getName() + " content")
+                                        .stars(9)
+                                        .pokemon(pokemon)
+                                        .build();
+                                reviewRepository.save(review2);
+
+                            });
+            System.out.println("#### Review Insert 끝");
+
+        };
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        //값이 null 이면 매핑 하지 않고 Skip 하는 설정
+        modelMapper.getConfiguration().setSkipNullEnabled(true);
+
+        return modelMapper;
+    }
+}
---
+++ src/main/java/com/pokemonreview/api/config/security/SecurityConfig.java
@@ -0,0 +1,98 @@
+package com.pokemonreview.api.config.security;
+
+import com.pokemonreview.api.exceptions.security.CustomAccessDeniedHandler;
+import com.pokemonreview.api.exceptions.security.JwtAuthEntryPoint;
+import com.pokemonreview.api.jwt.filter.JWTAuthenticationFilter;
+import com.pokemonreview.api.service.security.CustomUserDetailsService;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    private CustomUserDetailsService userDetailsService;
+
+    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return new JwtAuthEntryPoint();
+    }
+
+    @Bean
+    public JWTAuthenticationFilter jwtAuthenticationFilter() {
+        return new JWTAuthenticationFilter();
+    }
+
+    public CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowedOriginPatterns(Collections.singletonList("*")); // ⭐️ 허용할 origin
+            config.setAllowCredentials(true);
+            return config;
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
+                //.cors(withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/api/auth/**").permitAll()
+                            .requestMatchers("/api/pokemon/**", "/api/admin/**").authenticated();
+                })
+                //.formLogin(withDefaults())
+                //Session 정책을 STATELESS 로 설정
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                //JWTAuthenticationFilter 가 먼저 동작 하도록 설정
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(authManager -> authManager
+                        .authenticationEntryPoint(authenticationEntryPoint())
+                        .accessDeniedHandler(accessDeniedHandler())
+                )
+                .build();
+    }
+}
